f1d9f6e4352a6b3276b2f564eea250fa38265839
nkuek <nkuek1@gmail.com>
2021-02-01 15:52:08 -0500

    psql set up

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { asyncHandler } = require('./utils');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
 });
 
+router.post('/');
 module.exports = router;
