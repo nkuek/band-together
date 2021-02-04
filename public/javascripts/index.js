2cf3772e7fc211b130d48b3dfa62e769a4fc1fdb
nkuek <nkuek1@gmail.com>
2021-02-04 18:10:20 -0500

    attempting to do dots

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -32,42 +32,55 @@ window.addEventListener('DOMContentLoaded', (event) => {
             }
         });
 
-        document.querySelector('.note-delete').addEventListener('click', async (e) => {
+    document
+        .querySelector('.note-delete')
+        .addEventListener('click', async (e) => {
             e.preventDefault();
-            const deleteButton = document.querySelector('.note-delete')
-            
+            const deleteButton = document.querySelector('.note-delete');
 
             try {
-                const res = await fetch(
-                    deleteButton.href,
-                    {
-                        method: 'DELETE',
-                        headers: { 'Content-Type': 'application/json' }
-                    }
-                    );
-                    deleteButton.parentElement.innerHTML = ""
+                const res = await fetch(deleteButton.href, {
+                    method: 'DELETE',
+                    headers: { 'Content-Type': 'application/json' },
+                });
+                deleteButton.parentElement.innerHTML = '';
             } catch (e) {
                 console.error(e);
             }
         });
 
-        document.querySelector('.note-edit').addEventListener('click', async (e) => {
+    document
+        .querySelector('.songpost-note')
+        .addEventListener('click', async (e) => {
             e.preventDefault();
-            const editButton = document.querySelector('.node-edit')
-            const body = editButton.parentElement.value
+            const editButton = e.target;
+            const body = editButton.parentElement.value;
+            if (e.target.className === 'note-edit') {
+                try {
+                    const data = await fetch(editButton.href, {
+                        method: 'GET',
+                    });
 
-            try {
-                const res = await fetch(
-                    deleteButton.href,
-                    {
-                        method: 'DELETE',
-                        headers: { 'Content-Type': 'application/json' }
-                    }
-                    );
-                    deleteButton.parentElement.innerHTML = ""
-            } catch (e) {
-                console.error(e);
+                    const { songPostNote } = await data.json();
+                    const parent = editButton.parentElement;
+                    editButton.parentElement.innerHTML = '';
+                    const textArea = document.createElement('textarea');
+                    textArea.value = songPostNote.body;
+                    const update = document.createElement('a');
+                    update.innerHTML = 'update';
+                    const cancel = document.createElement('a');
+                    cancel.innerHTML = 'cancel';
+                    parent.appendChild(textArea);
+                    parent.appendChild(update);
+                    parent.appendChild(cancel);
+
+                    const res = await fetch(editButton.href, {
+                        method: 'PUT',
+                        headers: { 'Content-Type': 'application/json' },
+                    });
+                } catch (e) {
+                    console.error(e);
+                }
             }
         });
-
 });
