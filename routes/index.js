7f349bdc81002e003f03a359393d467d33e3e2a9
nkuek <nkuek1@gmail.com>
2021-02-05 18:57:50 -0500

    search bar working

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,30 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../db/models');
-const { asyncHandler } = require('./utils');
+const { asyncHandler, csrfProtection } = require('./utils');
 const { requireAuth } = require('../auth');
 
 /* GET home page. */
 router.get(
     '/',
+    csrfProtection,
     asyncHandler(async (req, res, next) => {
         const songPosts = await db.SongPost.findAll({
             order: [['createdAt', 'DESC']],
             limit: 10,
         });
-        res.render('home', { title: 'Welcome to Band-Together', songPosts });
+        res.render('home', {
+            title: 'Welcome to Band-Together',
+            songPosts,
+            csrfToken: req.csrfToken(),
+        });
     })
 );
+
+router.post('/', csrfProtection, requireAuth, (req, res) => {
+    console.log(req.body);
+    const { search } = req.body;
+    res.redirect(`/search/${search}`);
+});
 
 module.exports = router;
