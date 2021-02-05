9718b8c479f4233376a09c520754fc2f08bfbf4a
nkuek <nkuek1@gmail.com>
2021-02-05 13:00:14 -0500

    edit and delete formatting

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -17,14 +17,24 @@ window.addEventListener('DOMContentLoaded', (event) => {
                     }
                 );
                 const { note, username } = await res.json();
-                const notesContainer = document.querySelector(
-                    '.notes-container'
-                );
+                const notesContainer = document.querySelector('.songpost-note');
 
                 const newNote = document.createElement('div');
-
+                newNote.className = 'notes-posted';
                 newNote.innerHTML = `${username}: ${note.body}`;
-                notesContainer.appendChild(newNote);
+                const editBtn = document.createElement('a');
+                const deleteBtn = document.createElement('a');
+                editBtn.href = `/api/songposts/${note.songPostId}/notes/${note.id}/edit`;
+                editBtn.className = 'note-edit';
+                deleteBtn.className = 'note-delete';
+                editBtn.innerText = 'Edit';
+                deleteBtn.innerText = 'Delete';
+                deleteBtn.href = `/api/songposts/${note.songPostId}/notes/${note.id}/delete`;
+                newNote.appendChild(editBtn);
+                newNote.appendChild(deleteBtn);
+
+                // newNote.innerHTML = `<div class="notes-posted">${username}: ${note.body}</div><a href="/api/songposts/${note.songPostId}/notes/${note.id}/edit"> Edit <a href="/api/songposts/${note.songPostId}/notes/${note.id}/delete"> Delete`;
+                notesContainer.prepend(newNote);
                 document.querySelector('.noteText').value = '';
             } catch (e) {
                 console.error(e);
@@ -43,7 +53,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
                         method: 'DELETE',
                         headers: { 'Content-Type': 'application/json' },
                     });
-                    deleteButton.parentElement.innerHTML = '';
+                    deleteButton.parentElement.remove();
                 } catch (e) {
                     console.error(e);
                 }
@@ -59,6 +69,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
                     const oldText = JSON.parse(
                         JSON.stringify(parent.innerHTML)
                     );
+
                     const htmlElements = e.target.parentElement.querySelectorAll(
                         'a'
                     );
@@ -66,27 +77,30 @@ window.addEventListener('DOMContentLoaded', (event) => {
                     for (let i = 0; i < 2; i++) {
                         htmlString += htmlElements[i].outerHTML;
                     }
-                    editButton.parentElement.innerHTML = '';
-                    const textArea = document.createElement('textarea');
-                    textArea.value = songPostNote.body;
-                    const update = document.createElement('a');
-                    update.className = 'note-update-button';
-                    update.href = '/';
-                    update.innerHTML = 'update';
-                    const cancel = document.createElement('a');
-                    cancel.className = 'note-cancel-button';
-                    cancel.href = '/';
-                    cancel.innerHTML = 'cancel';
-                    parent.appendChild(textArea);
-                    parent.appendChild(update);
-                    parent.appendChild(cancel);
+                    editButton.parentElement.innerHTML = `<textarea class="text-area">${songPostNote.body}</textarea> <a href="/" class='note-update-button'>Update <a href="/" class="note-cancel-button">Cancel`;
+                    // const textArea = document.createElement('textarea');
+                    // textArea.value = songPostNote.body;
+                    // const update = document.createElement('a');
+                    // update.className = 'note-update-button';
+                    // update.href = '/';
+                    // update.innerHTML = 'update';
+                    // const cancel = document.createElement('a');
+                    // cancel.className = 'note-cancel-button';
+                    // cancel.href = '/';
+                    // cancel.innerHTML = 'cancel';
+                    // parent.appendChild(textArea);
+                    // parent.appendChild(update);
+                    // parent.appendChild(cancel);
 
                     parent.addEventListener('click', async (e) => {
                         e.preventDefault();
+                        const textArea = document.querySelector('.text-area');
                         if (e.target.className === 'note-cancel-button') {
                             parent.innerHTML = oldText;
                         }
                         if (e.target.className === 'note-update-button') {
+                            console.log(editButton.href);
+                            console.log(textArea.value);
                             const res = await fetch(editButton.href, {
                                 method: 'PUT',
                                 headers: { 'Content-Type': 'application/json' },
